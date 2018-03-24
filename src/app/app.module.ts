c365bbccb1c00200d591cbe20f0b0f05aff61205
Lucas Assmann <l.assmann.30@gmail.com>
2018-03-24 20:16:31 -0300

    first proposal structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
 import {MaterialModule} from './material/material.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
